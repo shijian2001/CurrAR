7a3b3848b831a89f9435317f9bbfda4151c82e1b
zqqqqz2000 <zqqqqz2000@sina.cn>
2025-05-25 16:27:06 +0800

    fix: remove useless data

--- simpar/train/curriculum.py
+++ simpar/train/curriculum.py
@@ -98,7 +98,10 @@ class CurriculumPromptLoader:
         if self.difficulty_to_prompts_idx[self.current_difficulty] >= len(
             self.difficulty_to_prompts[self.current_difficulty]
         ):
-            logger.warning(f"difficulty {self.current_difficulty} has no more prompts, reset to 0")
+            logger.warning(f"difficulty {self.current_difficulty} has no more prompts, increase")
+            assert self.difficulty_range
+            self.current_difficulty = min(self.current_difficulty + 1, self.difficulty_range[1])
+            logger.warning(f"difficulty will to {self.current_difficulty}")
             self.difficulty_to_prompts_idx[self.current_difficulty] = self.accelerator.process_index
         prompt = self.difficulty_to_prompts[self.current_difficulty][
             self.difficulty_to_prompts_idx[self.current_difficulty]
